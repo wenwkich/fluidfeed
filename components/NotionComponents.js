240bcb2bbaa4d43e40a1e42d71aea4cf730ed2d6
Wendy Chan <wenwkich@gmail.com>
2022-04-02 23:37:01 +0800

    remove imports

--- components/NotionComponents.js
+++ components/NotionComponents.js
@@ -4,9 +4,6 @@ import { NotionRenderer } from 'react-notion-x'
 import dynamic from 'next/dynamic'
 
 const Code = dynamic(() => import('react-notion-x/build/third-party/code').then((m) => m.Code))
-const Collection = dynamic(() =>
-  import('react-notion-x/build/third-party/collection').then((m) => m.Collection)
-)
 const Equation = dynamic(() =>
   import('react-notion-x/build/third-party/equation').then((m) => m.Equation)
 )
